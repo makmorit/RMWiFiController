f6c9cc1cb7b5e95322496105d536967ddd417947
Makoto Morita <makmorit@outlook.jp>
2017-05-16 21:43:37 +0900

    定数を変更
    SSID／パスフレーズ／IPアドレスを、Wi-Fi HTTPサーバーに固有な内容へ変更

--- AE_ESP_board/WiFiHTTPBoard/WiFiInfo.h
+++ AE_ESP_board/WiFiHTTPBoard/WiFiInfo.h
@@ -1,11 +1,11 @@
 #ifndef __WIFIINFO_H
 #define __WIFIINFO_H
 
-#define WIFI_SSID  "XXXX"
-#define WIFI_PSWD "YYYY"
+#define WIFI_SSID  "RMWiFiHTTPBoard"
+#define WIFI_PSWD "aab5ca713f808f5b8a8fb0b5a3df9e6b"
 
-#define WIFI_SVR_IPADDR  "192.168.XXX.YYY"
-#define WIFI_SVR_IPADDR_FN  (IPAddress(192,168,XXX,YYY))
+#define WIFI_SVR_IPADDR  "192.168.253.1"
+#define WIFI_SVR_IPADDR_FN  (IPAddress(192,168,253,1))
 #define WIFI_SVR_PORTNO  80
 
 #endif // __WIFIINFO_H
